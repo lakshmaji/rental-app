0fc524d110d853181f1ea6757484da10c4c5fb46
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-10-22 15:08:14 +0530

    hook rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,8 +15,11 @@ module.exports = {
     project: ['tsconfig.json']
   },
   plugins: [
-    'react'
+    'react',
+    'react-hooks'
   ],
   rules: {
+    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+    "react-hooks/exhaustive-deps": "warn" // Checks effect dependencies
   }
 }
