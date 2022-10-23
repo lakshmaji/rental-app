011fc0b5a9b10e962d944cc33da03c94fb4a4c65
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-10-23 11:17:08 +0530

    refactor: use compressed images

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,5 +1,5 @@
-import image1 from '../assets/explore/pexels-dmitry-zvolskiy-2082087 1.webp'
-import image2 from '../assets/explore/pexels-naim-benjelloun-2029670 1.webp'
+import image1 from '../assets/explore/one.webp'
+import image2 from '../assets/explore/two.webp'
 import { IProduct } from '../models/product'
 
 const products: IProduct[] = [
@@ -8,16 +8,15 @@ const products: IProduct[] = [
     name: 'Room with one king-size bed',
     price: 35,
     area: 28,
-    image: image1
+    image: image1,
   },
   {
     id: 2,
     name: 'Penthouse for 8 person',
     price: 2039,
     area: 438,
-    image: image2
-
-  }
+    image: image2,
+  },
 ]
 
 export default products
